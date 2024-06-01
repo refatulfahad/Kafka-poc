7f114aa99982f01a410db833aacc098c6e953e8b
refatulfahad <refatm452@gmail.com>
2024-06-01 11:02:40 +0600

    Add project files.

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,10 @@
+﻿using KafkaConsumer;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ConsumeMessage consumeMessage = new ConsumeMessage();
+        consumeMessage.ReadMessage();
+    }
+}
---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,10 @@
+﻿using KafkaProducer;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ProduceMessage produceMessage = new ProduceMessage();
+        produceMessage.CreateMessage().Wait();
+    }
+}
